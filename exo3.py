10667a3ca6576c587fc9b5ef01a87badc588c854
Zanehisako <yassinetakiko@gmail.com>
2024-10-12 01:55:34 +0100

    its works with the possible N

--- exo3.py
+++ exo3.py
@@ -5,14 +5,21 @@ def calculate_surface(i,j,k):
 
 def calculate_surface_max_min(N):
     combinations = []
-    for i in range(1,N):
-        for j in range(1,N-i):
+    for i in range(0,N):
+        i= i+1
+        j = 1
+        while True:
             k = math.floor(N/(i*j))
             if (i*j*k) == N:
                 print('the combinations are :',i,j,k)
                 surface = calculate_surface(i,j,k);
                 print('the result is :',surface)
                 combinations.append(surface) 
+            if j>N-1:
+               break 
+            else:
+                j=j+1
+
     max_surface = max(combinations)
     min_surface = min(combinations)
 
